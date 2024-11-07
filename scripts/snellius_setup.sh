0c2ee0977a54a25ee98ad5663e2df1ba1dceda16
Miklos Hamar <miklos.hamar@student.uva.nl>
2024-11-07 22:27:44 +0100

    fixes to environment setup

--- scripts/snellius_setup.sh
+++ scripts/snellius_setup.sh
@@ -7,5 +7,10 @@ setup() {
 	module load Java/21.0.2
 
 	source "$1/.venv/bin/activate"
+
+	echo "Environment setup complete."
+	echo "Python version: $(python --version)"
+	echo "Java version: $(java --version)"
+	echo "Java compiler version: $(javac --version)"
 }
 
