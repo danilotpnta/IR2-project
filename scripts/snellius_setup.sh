65b8b39a7d747c8acdaa34b3e528f568e31d6641
danilotpnta <danilotpnta@gmail.com>
2024-11-07 23:30:06 +0100

    udpate based on reproducibility branch, Java needed for faiss-cpu

--- scripts/snellius_setup.sh
+++ scripts/snellius_setup.sh
@@ -4,7 +4,12 @@ setup() {
 	module purge
 	module load 2024
 	module load Python/3.12.3-GCCcore-13.3.0
+	module load Java/21.0.2 
 
 	source "$1/.venv/bin/activate"
-}
 
+	echo "Environment setup complete."
+	echo "Python version: $(python --version)"
+	echo "Java version: $(java --version)"
+	echo "Java compiler version: $(javac --version)"
+}
