82597b8507ece5ca309155e94add824697645761
danilotpnta <danilotpnta@gmail.com>
2024-11-20 13:14:52 +0100

    Get same results as InPars + add new prompt strategies

--- scripts/snellius_setup.sh
+++ scripts/snellius_setup.sh
@@ -3,8 +3,8 @@
 setup() {
 	module purge
 	module load 2024
-	module load Python/3.12.3-GCCcore-13.3.0
 	module load Java/21.0.2 
+	module load Python/3.12.3-GCCcore-13.3.0
 
 	source "$1/.venv/bin/activate"
 
