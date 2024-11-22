58b6c78a1c9806ffe71d3f80bebe0f80012593b9
JesseBrouw <jessebrouwers@hotmail.nl>
2024-11-22 09:38:03 +0100

    added llama3.1 results

--- scripts/snellius_setup.sh
+++ scripts/snellius_setup.sh
@@ -3,17 +3,20 @@
 setup() {
 	module purge
 	
-	# Python 3.10 setup, for evaluation (pyserini needs 3.10)
-	module load 2022
-	module load Python/3.10.4-GCCcore-11.3.0
+	module load 2023
 	module load 2024
+	module load Python/3.11.3-GCCcore-12.3.0
 	module load Java/21.0.2
+	
 	source "$1/.venv/bin/activate"
 
 	export CUDA_VISIBLE_DEVICES=0
 	export TF_ENABLE_ONEDNN_OPTS=0
 	export WANDB_API_KEY=
 	export HF_TOKEN=
+	
+	# The pretrained MonoT5 models are >22GB, so cache in scratch-shared
+	export TRANSFORMERS_CACHE="/scratch-shared/InPars-data/HF_Cache"
 
 	echo "Environment setup complete."
 	echo "Python version: $(python --version)"
