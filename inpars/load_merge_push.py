d0970ed01c6ebd48983f811187b195dc54a4fe09
Miklos Hamar <miklos.hamar@student.uva.nl>
2024-12-11 19:14:52 +0100

    remove garbage

--- inpars/load_merge_push.py
+++
@@ -1,6 +0,0 @@
-from unsloth import FastLanguageModel
-
-def load_and_merge(model_name, checkpoint):
-    model: FastLanguageModel = FastLanguageModel.from_pretrained(model_name)
-    
-    return model
