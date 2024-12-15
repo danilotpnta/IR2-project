a62189fa9ae1da684e8e5cb0b91b039f6394de91
danilotpnta <danilotpnta@gmail.com>
2024-12-15 16:16:35 +0100

    rm model porting to CONFIG dir

--- inpars/config.py
+++ inpars/config.py
@@ -7,19 +7,3 @@ ROOT_DIR = Path(__file__).resolve().parent.parent
 # Define directories based on ROOT_DIR
 DATA_DIR = ROOT_DIR / "data"
 DATASETS_DIR = DATA_DIR / "datasets"
-
-# Ports used by the model servers
-MODEL_PORT_MAPPING = {
-    "EleutherAI/gpt-j-6B": 8000,
-    "meta-llama/Llama-3.1-8B": 8001,
-    "neuralmagic/Llama-3.1-Nemotron-70B-Instruct-HF-FP8-dynamic": 8002,
-}
-
-# Context Length
-MAX_TOKENS = {
-    "EleutherAI/gpt-j-6B": 2048,
-    "meta-llama/Llama-3.1-8B": 8192, 
-    "neuralmagic/Llama-3.1-Nemotron-70B-Instruct-HF-FP8-dynamic": 8192,
-}
-
-STOP_WORDS = ["\n", "\n\n", "Bad Question:", "Example", "Document:"]
